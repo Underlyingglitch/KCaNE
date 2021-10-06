d1554706b707ff2a5bb9fac4a020ab76613c4c51
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2021-10-06 17:04:29 +0200

    Search function update

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,16 +1,16 @@
-$(function(){
-    $('#search').on('keyup', function(){
-        if ($(this).val().length > 0) {
-            var i = 0;
-            $.each($('h2'), function(){
-                $(this).closest('.col-md-2').hide().filter(function(){
-                    return $(this).text().toLowerCase().lastIndexOf($('#search').val().toLowerCase(),0)==0;
-                }).show("fast", function(){i++;});
-            });
-        } else {
-            $('h2').closest('.col-md-2').show();
-        }
-        var total = $('h2').length;
-    });
-    
+$(() => {
+  $('#search').on('keyup', function () {
+    if ($(this).val().length > 0) {
+      let i = 0;
+      $.each($('h2'), function () {
+        $(this).closest('.col-md-2').hide().filter(function () {
+          return $(this).text().toLowerCase().lastIndexOf($('#search').val().toLowerCase(), 0) == 0;
+        })
+          .show('fast', () => { i++; });
+      });
+    } else {
+      $('h2').closest('.col-md-2').show();
+    }
+    const total = $('h2').length;
+  });
 });
